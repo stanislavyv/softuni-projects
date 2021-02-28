0f44dd1d6c72e83b9893b3054a0898b4eba57357
Stanislav Valkov <63970354+stanislavyv@users.noreply.github.com>
2021-02-28 20:14:51 +0200

    Added files.

---
+++ Custom Doubly Linked List/Custom Doubly Linked List/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Custom_Doubly_Linked_List
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
