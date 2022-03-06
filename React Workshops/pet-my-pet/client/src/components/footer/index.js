3776463a0f8cc08b42f643451ceeeb7644105e37
Stanislav Valkov <stanislavyv31@gmail.com>
2022-03-06 02:05:00 +0200

    Implement footer component

---
+++ React Workshops/pet-my-pet/client/src/components/footer/index.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer id="site-footer">
+            <p>@PetMyPet</p>
+            <style jsx>
+                
+            </style>
+        </footer>
+    );
+}
+
+export default Footer;
