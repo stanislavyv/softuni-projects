aaf8e708e8802713c8e19971357a69e64cee7403
Stanislav Valkov <stanislavyv31@gmail.com>
2022-03-23 23:00:49 +0200

    feat: My pets list component

---
+++ React Workshops/pet-my-pet/client/src/components/my-pets-list/index.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import petService from '../../utils/petService'
+import PetCard from '../pet-card';
+
+const MyPetsList = ({ username }) => {
+    const [pets, setPets] = useState([]);
+
+    useEffect(() => {
+        setPets(getUserPets);
+    }, []);
+    
+    const getUserPets = (pets) => {
+        petService
+            .getAll()
+            .then(pets => {
+                return pets.filter(p => p.creator === username)
+            })
+    };
+
+    return (
+        <ul className="my-pets-list">
+            {pets.forEach(p => {
+                <PetCard {...p} />
+            })}
+        </ul>
+    );
+}
+
+export default MyPetsList;
